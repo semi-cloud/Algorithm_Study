67cfbe3fed5d0310d9d357a92c897ddcca1ada11
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2021-07-20 18:03:27 +0900

    Update Greedy1744.java

--- Greedy/Greedy1744.java
+++ Greedy/Greedy1744.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 	static int n;    
